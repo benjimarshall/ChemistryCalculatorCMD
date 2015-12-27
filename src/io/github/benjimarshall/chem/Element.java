4d8237ef04aa66761b9dcb5bb1548fba2413924a
Benji Marshall <benji.john.marshall@gmail.com>
2015-12-27 17:41:51 +0000

    Added periodic table reader and constructors to use this. NB: No Errors are thrown

--- src/io/github/benjimarshall/chem/Element.java
+++ src/io/github/benjimarshall/chem/Element.java
@@ -1,16 +1,58 @@
 package io.github.benjimarshall.chem;
 
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Element {
     public Element() {
 
     }
 
-    public Element(String name, int flag) {
-        /*
-        SYMBOL = 0
-        NAME = 1
-        */
+    public Element(Element element) {
+        this.symbol = element.getSymbol();
+        this.name = element.getName();
+        this.atomicNumber = element.getAtomicNumber();
+        this.massNumber = element.getMassNumber();
+    }
 
+    public Element(String name, int flag) {
+        boolean successful = false;
+        if (flag == SYMBOL) {
+            for (Element element : periodicTable) {
+                if (element.getSymbol() == name) {
+                    this.symbol = element.getSymbol();
+                    this.name = element.getName();
+                    this.atomicNumber = element.getAtomicNumber();
+                    this.massNumber = element.getMassNumber();
+                    successful = true;
+                    break;
+                }
+            }
+        }
+        else if (flag == NAME) {
+            for (Element element : periodicTable) {
+                if (element.getName() == name) {
+                    this.symbol = element.getSymbol();
+                    this.name = element.getName();
+                    this.atomicNumber = element.getAtomicNumber();
+                    this.massNumber = element.getMassNumber();
+                    successful = true;
+                    break;
+                }
+            }
+        }
+
+    }
+
+    public Element(String symbol, String name, int atomicNumber, double massNumber) {
+        this.symbol = symbol;
+        this.name = name;
+        this.atomicNumber = atomicNumber;
+        this.massNumber = massNumber;
     }
 
     public String getSymbol() {
@@ -37,5 +79,31 @@ public class Element {
     public final static int SYMBOL = 0;
     public final static int NAME = 1;
 
+    // Make the periodic table
+    private static ArrayList<Element> periodicTable = new ArrayList<>();
+    static {
+        try {
+            // Make the CSV reader
+            InputStream is = (new Element()).getClass().getResourceAsStream("/periodic_table.csv");
+            CSVReader r = new CSVReader(new InputStreamReader(is));
 
+            // Dump the header row
+            r.readNext();
+
+            // For each line make the respective element
+            for (String line[]: r) {
+                periodicTable.add(new Element(
+                        line[1], // Symbol
+                        line[2], // Name
+                        Integer.parseInt(line[0]), // Atomic Number
+                        Double.parseDouble(line[3]) // Mass Number
+                ));
+            }
+            r.close();
+        }
+        catch (IOException e) {
+            System.out.println("There has been an IO error in reading the periodic table. Program closing");
+            System.exit(1);
+        }
+    }
 }
