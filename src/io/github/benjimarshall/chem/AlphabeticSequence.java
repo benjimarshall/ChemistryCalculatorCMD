2c845c3aa4c181cec84c42410220488df00322b8
Benji Marshall <benji.john.marshall@gmail.com>
2016-02-05 10:14:01 +0000

    Added a Javadoc for the AlphabeticSequence class

--- src/io/github/benjimarshall/chem/AlphabeticSequence.java
+++ src/io/github/benjimarshall/chem/AlphabeticSequence.java
@@ -2,8 +2,15 @@ package io.github.benjimarshall.chem;
 
 import java.util.Iterator;
 
+/**
+ * An Alphabetic Sequence generator. For example: A, B, C, D, ..., Y, Z, AA, AB, AC, ..., AY, AZ, BA, BC, ... ZX, ZY,
+ * ZZ, AAA, AAB, AAC, ...
+ */
 public class AlphabeticSequence implements Iterator<String> {
-    public  static char[] alphabet = new char[26];
+    /**
+     * An array of characters of the alphabet, in order
+     */
+    public static char[] alphabet = new char[26];
     // Generate an alphabet
     static{
         final String stringAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -13,13 +20,22 @@ public class AlphabeticSequence implements Iterator<String> {
         }
     }
 
-    protected int position = -1;
-    protected String prefix = "";
+    private int position = -1;
 
+    /**
+     * Implements {@link Iterator}'s method to see if there is another word in the sequence, which there always will be
+     * @return if there is another word in the sequence, {@code true}
+     */
     public boolean hasNext() {
         return true;
     }
 
+    /**
+     * Finds the next element for this position in the sequence
+     * @param prefix the position of the element in the sequence
+     * @param suffix the characters after this in the element
+     * @return the element in the sequence
+     */
     private StringBuilder makeString(int prefix, StringBuilder suffix) {
         // If the number is more than 26, another letter is needed
         if (prefix >= 26)  {
@@ -35,6 +51,10 @@ public class AlphabeticSequence implements Iterator<String> {
         return suffix;
     }
 
+    /**
+     * Gets the next element in the sequence
+     * @return the next element in the sequence
+     */
     public String next() {
         position++;
         return makeString(position, new StringBuilder()).toString();
